2165901808c70545c95cf5bce7bfea8824a1c31d
James William Fletcher <sirtames@gmail.com>
2018-10-08 04:35:13 +0100

    Added Nodeos 1.2.5 Builds

---
+++ Build/Nodeos 1.2.5/contour/contour.cpp
@@ -0,0 +1,127 @@
+/*
+      1st & 2nd price auction model smart contract for EOS
+      By James William Fletcher (~2018)
+      http://github.com/mrbid
+*/
+#include <eosiolib/eosio.hpp>
+
+class auction : public eosio::contract
+{
+
+public:
+
+      //Constructor
+      auction(account_name s): contract(s), _mem(s, s)
+      {
+            //Init only memory block required, make contract owner pay for the ram
+            _mem.emplace(_self, [&](auto& r)
+            {
+                  r.auctioneer = _self;
+                  r.winner = NULL;
+                  r.hb1 = 0;
+                  r.hb2 = 0;
+                  r.done = false;
+            });
+      }
+      
+      //Resets the auction
+      void reset(account_name owner)
+      {
+            require_auth(owner);
+            auto r = _mem.begin();
+            if(r->auctioneer == owner)
+            {
+                  _mem.modify(r, _self, [&](auto& w)
+                  {
+                        w.winner = NULL;
+                        w.hb1 = 0;
+                        w.hb2 = 0;
+                        w.done = false;
+                  });  
+            }
+      }
+
+      //Places a bid
+      void placebid(account_name owner, int64_t bid)
+      {
+            require_auth(owner);
+            
+            //Get memory
+            auto r = _mem.begin();
+            
+            //Is the auction done?
+            if(r->done == true)
+            {
+                  eosio::print("Sorry this auction has finished.\n");
+                  return;
+            }
+
+            //Is this bid high enough? 
+            if(r->hb1 > bid)
+            {
+                  eosio::print("Your bid is too low, it's outbid.\n\nHighest Bid: ", r->hb1, "\nSecond Highest Bid: ", r->hb2, "\n");
+                  return;
+            }
+
+            //Write mem
+            _mem.modify(r, _self, [&](auto& w)
+            {
+                  //Set current winner (highest bidder)
+                  w.winner = owner;
+
+                  //Set new highest bid (and last highest)
+                  w.hb2 = r->hb1;
+                  w.hb1 = bid;
+            });
+      }
+
+      //Finish the auction and set the winner  
+      void endauction(account_name owner)
+      {
+            require_auth(owner);
+            auto r = _mem.begin();
+            _mem.modify(r, _self, [&](auto& w)
+            {
+                  w.done = true;
+            });
+            getwinner(owner);
+      }
+
+      //Who was the winner / who is the current winner?
+      void getwinner(account_name owner)
+      {
+            require_auth(owner);
+            auto r = _mem.begin();
+            if(r->auctioneer == owner)
+            {
+                  eosio::print("The winning address: ", r->winner, "\nHighest Bid: ", r->hb1, "\nSecond Highest Bid: ", r->hb2, "\n\n");
+                  if(r->done == false)
+                        eosio::print("The auction has not finished yet.\n");
+                  else
+                        eosio::print("This auction has finished.\n");
+            }
+            else
+            {
+                  eosio::print("You need to be the Auctioneer to see the winner.\n");
+            }
+      }
+
+private:
+
+      struct record
+      {
+            account_name auctioneer; //Auctioneer Address (Makes sure only the Auction `initiater` can end the Auction)
+            account_name winner; //Winning Address
+            uint64_t hb2; //Highest Bid (2nd price)
+            uint64_t hb1; //Highest Bid (1st price)
+            bool done;
+
+            uint64_t primary_key() const{return winner;}
+      };
+
+      typedef eosio::multi_index<N(records), record> mem_table;
+      mem_table _mem;
+
+};
+
+EOSIO_ABI(auction, (placebid)(endauction)(getwinner)(reset))
---
+++ Build/Nodeos 1.2.5/pontour/pontour.cpp
@@ -0,0 +1,108 @@
+/*
+      1st & 2nd price auction model smart contract for EOS
+      By James William Fletcher (~2018)
+      http://github.com/mrbid
+*/
+#include <eosiolib/eosio.hpp>
+
+class auction : public eosio::contract
+{
+
+public:
+
+      //Constructor
+      auction(account_name s): contract(s), _bids(s, s) {}
+
+      //Get 1st and 2nd bids
+      void sync()
+      {
+            _hb1 = 0;
+            _hb2 = 0;
+            _winner = 0;
+            for(auto it = _bids.begin(); it != _bids.end(); ++it)
+            {
+                  if(it->bid > _hb1)
+                  {
+                        _hb2 = _hb1;
+                        _hb1 = it->bid;
+                        _winner = it->owner;
+                  }
+            }
+      }
+
+      //Places a bid
+      void placebid(account_name owner, int64_t bid)
+      {
+            require_auth(owner);
+            
+            //Sync local variables from persistent storage
+            sync();
+
+            //Is this bid high enough? 
+            if(_hb1 > bid)
+            {
+                  eosio::print("Your bid is too low, it's outbid.\n\nHighest Bid: ", _hb1, "\nSecond Highest Bid: ", _hb2, "\n");
+                  return;
+            }
+
+            //Set new highest bid (and last highest)
+            _hb2 = _hb1;
+            _hb1 = bid;
+
+            //If record exists delete it
+            for(auto it = _bids.begin(); it != _bids.end();)
+            {
+                  if(it->owner == owner)
+                  {   
+                        _bids.erase(it);
+                        break;
+                  }
+                  else
+                  {
+                        ++it;
+                  }       
+            }    
+
+            //Create new bid record
+            _bids.emplace(owner, [&](auto& rcrd)
+            {
+                  rcrd.owner = owner;
+                  rcrd.bid = bid;
+            });
+      }
+
+      //Who was the winner / who is the current winner?
+      void getwinner(account_name owner)
+      {
+            require_auth(owner);
+            sync();
+            eosio::print("The winning address: ", _winner, "\nHighest Bid: ", _hb1, "\nSecond Highest Bid: ", _hb2, "\n\n");
+      }
+      
+      //Dump memory (all bids and addresses)
+      void getbids(account_name owner)
+      {
+            require_auth(owner);
+            for(auto it = _bids.begin(); it != _bids.end(); ++it)
+                  eosio::print("Address: ", it->owner, " - Bid:", it->bid, "\n");
+      }
+
+private:
+
+      uint64_t _hb2;
+      uint64_t _hb1;
+      account_name _winner;
+      
+      struct record
+      {
+            account_name owner;
+            int64_t bid;
+            uint64_t primary_key() const{return owner;}
+      };
+
+      typedef eosio::multi_index<N(records), record> bids_table;
+      bids_table _bids;
+
+};
+
+EOSIO_ABI(auction, (placebid)(getwinner)(getbids))
